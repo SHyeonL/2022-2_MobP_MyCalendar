ea120dfad2cffc202e2b6eac9dd1bcd8d554a9ae
SHyeonL <kjghost1@naver.com>
2022-11-27 12:13:10 +0900

    캘린더 레이아웃 수정

--- MyCalendar/app/src/main/java/com/example/mycalendar/ui/Calendar/CalendarFragment.java
+++ MyCalendar/app/src/main/java/com/example/mycalendar/ui/Calendar/CalendarFragment.java
@@ -3,6 +3,9 @@ package com.example.mycalendar.ui.Calendar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -12,7 +15,10 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.mycalendar.MainActivity;
+import com.example.mycalendar.R;
 import com.example.mycalendar.databinding.FragmentCalendarBinding;
+import com.example.mycalendar.ui.Contact.AddContactActivity;
+import com.example.mycalendar.ui.SearchActivity;
 
 public class CalendarFragment extends Fragment {
 
@@ -25,14 +31,43 @@ public class CalendarFragment extends Fragment {
         binding = FragmentCalendarBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        binding.button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), AddScheduleActivity.class);
-                startActivity(intent);
-            }
-        });
+//        binding.button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(getActivity(), AddScheduleActivity.class);
+//                startActivity(intent);
+//            }
+//        });
         return root;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.common_action_bar, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                StartSearchActivity();
+                break;
+            case R.id.action_add:
+                StartAddScheduleActivity();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void StartAddScheduleActivity() {
+        Intent intent = new Intent(getActivity(), AddScheduleActivity.class);
+        startActivity(intent);
+    }
+
+    public void StartSearchActivity() {
+        Intent intent = new Intent(getActivity(), SearchActivity.class);
+        startActivity(intent);
     }
 
     @Override
