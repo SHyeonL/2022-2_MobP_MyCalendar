929738e3d6354b4ef8156fc813aced757818edec
SHyeonL <kjghost1@naver.com>
2022-12-04 18:04:54 +0900

    연락처 상세보기 액션 바 추가

--- MyCalendar/app/src/main/java/com/example/mycalendar/ui/Contact/DetailContactViewActivity.java
+++ MyCalendar/app/src/main/java/com/example/mycalendar/ui/Contact/DetailContactViewActivity.java
@@ -1,11 +1,15 @@
 package com.example.mycalendar.ui.Contact;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.mycalendar.R;
 import com.example.mycalendar.databinding.ActivityDetailContactViewBinding;
@@ -31,6 +35,35 @@ public class DetailContactViewActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.modify_action_bar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_edit:
+                break;
+            case R.id.action_delete:
+                AlertDialog.Builder menu = new AlertDialog.Builder(this);
+                menu.setTitle("연락처 삭제");
+                menu.setMessage("정말로 연락처를 삭제하시겠습니까?");
+                menu.setPositiveButton("삭제", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                        onBackPressed();
+                    }
+                });
+                menu.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+                menu.show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
